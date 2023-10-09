b6493aac41b69d4f94fa18c6af0edabba67cefb3
Shamkhal <shamkhalhuseynzade26@gmail.com>
2023-10-09 19:12:07 +0300

    test updated for deploy

--- src/main/java/com/project/forvi/controller/AuthenticationController.java
+++ src/main/java/com/project/forvi/controller/AuthenticationController.java
@@ -5,10 +5,7 @@ import com.project.forvi.dto.RegisterDto;
 import com.project.forvi.response.AuthenticationResponse;
 import com.project.forvi.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -33,6 +30,11 @@ public class AuthenticationController{
     ) {
 
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello world";
     }
 
 
